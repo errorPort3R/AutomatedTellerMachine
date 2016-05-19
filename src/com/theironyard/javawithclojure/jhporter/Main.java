ebf612b3c030f1662de8cd5f68d7b16d7fe56b66
jeffryporter <jporter7@gmail.com>
2016-05-19 09:38:41 -0400

    minor bug fix.

--- src/com/theironyard/javawithclojure/jhporter/Main.java
+++ src/com/theironyard/javawithclojure/jhporter/Main.java
@@ -8,7 +8,7 @@ public class Main
 
     public boolean isValidCustomer(String name)
     {
-        return name != " ";
+        return !name.equals(" ");
     }
 
     public static void main(String[] args) throws Exception
